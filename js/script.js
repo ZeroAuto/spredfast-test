10590870100c606202522a0381d2ff8167a74b5a
Mike James <mikejames386@gmail.com>
2017-04-04 15:36:46 -0500

    add setinterval function

--- js/script.js
+++ js/script.js
@@ -10,29 +10,34 @@
 
   var myPoll = new window.spredfast.Poller();
 
-  var resultsArray = [];
+  function executeCall() {
+    var resultsArray = [];
 
-  myPoll.poll({type:'fruits', frequency:15}, function(results) {
+    myPoll.poll({type:'fruits', }, function(results) {
 
-    for (var i = 0; i < results.length; i ++) {
-      resultsArray.push(results[i]);
-    }
+      for (var i = 0; i < results.length; i ++) {
+        resultsArray.push(results[i]);
+      }
 
-  });
+    });
 
-  myPoll.poll({frequency:15}, function(results) {
+    myPoll.poll({}, function(results) {
 
-    for (var i = 0; i < results.length; i ++) {
-      resultsArray.push(results[i]);
-    }
+      for (var i = 0; i < results.length; i ++) {
+        resultsArray.push(results[i]);
+      }
 
-    resultsArray.sort(compare);
+      resultsArray.sort(compare);
 
-    // I could have probably done this better with a loop but I was pressed for time
-    $('.first').html(resultsArray[0].name + " <span class='count-mentions'>" + resultsArray[0].count + " Mentions</span>");
-    $('.second').html(resultsArray[1].name + " <span class='count-mentions'>" + resultsArray[1].count + " Mentions</span>");
-    $('.third').html(resultsArray[2].name + " <span class='count-mentions'>" + resultsArray[2].count + " Mentions</span>");
-    $('.fourth').html(resultsArray[3].name + " <span class='count-mentions'>" + resultsArray[3].count + " Mentions</span>");
-    $('.fifth').html(resultsArray[4].name + " <span class='count-mentions'>" + resultsArray[4].count + " Mentions</span>");
-  });
+      // I could have probably done this better with a loop but I was pressed for time
+      $('.first').html(resultsArray[0].name + " <span class='count-mentions'>" + resultsArray[0].count + " Mentions</span>");
+      $('.second').html(resultsArray[1].name + " <span class='count-mentions'>" + resultsArray[1].count + " Mentions</span>");
+      $('.third').html(resultsArray[2].name + " <span class='count-mentions'>" + resultsArray[2].count + " Mentions</span>");
+      $('.fourth').html(resultsArray[3].name + " <span class='count-mentions'>" + resultsArray[3].count + " Mentions</span>");
+      $('.fifth').html(resultsArray[4].name + " <span class='count-mentions'>" + resultsArray[4].count + " Mentions</span>");
+    });
+  }
+
+  executeCall();
+  window.setInterval(executeCall, 15000);
 }(jQuery));
